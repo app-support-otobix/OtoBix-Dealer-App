d8adeba838e009a501d61faf9537b8023af94bc4
muhammadahsankz <ahsanshahidkhanzada@gmail.com>
2026-06-24 13:55:33 +0500

    Fixed New Fields Issue 2

--- lib/Utils/app_constants.dart
+++ lib/Utils/app_constants.dart
@@ -3,7 +3,7 @@ enum DeploymentEnvironment { local, dev, prod }
 class AppConstants {
   // ---- Deploy on Production or Development by changing this ----
   static const DeploymentEnvironment deploymentEnvironment =
-      DeploymentEnvironment.dev;
+      DeploymentEnvironment.prod;
 
   // Other constant classes
   static final Roles roles = Roles();
